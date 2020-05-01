651c9a7f65e7334955fad1aefb44c49ddf09d50f
Kiel Velasco <KVUni09@gmail.com>
2020-04-30 21:18:46 -0500

    added Projector

--- src/assetmanagementsystem/model/Projector.java
+++ src/assetmanagementsystem/model/Projector.java
@@ -1,16 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package assetmanagementsystem.model;
 
 /**
  *
- * @author kyle
+ * @author Kiel Roi Velasco
  */
 public class Projector extends TechnologyAsset {
-    
+
     public Projector(String idNumber, String name, String location, String usedBy, AssetState assetState){
         super.type=TechnologyType.AUDIO_OR_VIDEO;
         super.id=idNumber;
@@ -23,14 +19,20 @@ public class Projector extends TechnologyAsset {
 
     @Override
     public String configureIPAddress() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        super.IPAddress=IPAddressConfigurer.getInstance().staticIPAddresses.remove(0);
+        return "Your new static IP adress is "+super.IPAddress;
+    }
+
+    public void giveBackIPAddress(){
+        IPAddressConfigurer.getInstance().staticIPAddresses.add(super.IPAddress);
+        super.IPAddress="";
     }
 
     @Override
     public String performMaintenance() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return "Cleaning projector window and replacing projector lamp.";
     }
-    
+
     public String project(){
         return "They're really just dots on the wall.";
     }
