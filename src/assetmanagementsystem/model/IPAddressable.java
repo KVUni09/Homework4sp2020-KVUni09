8c179df38a563693dfc98a52c52573edf98cd44d
Kiel Velasco <KVUni09@gmail.com>
2020-04-30 21:41:38 -0500

    finalizing

--- src/assetmanagementsystem/model/IPAddressable.java
+++ src/assetmanagementsystem/model/IPAddressable.java
@@ -1,15 +1,8 @@
 package assetmanagementsystem.model;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
 /**
  *
- * @author kyle
+ * @author Kiel Roi Velasco
  */
 public interface IPAddressable {
     public String configureIPAddress();
