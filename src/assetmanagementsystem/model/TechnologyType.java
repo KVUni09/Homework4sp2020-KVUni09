edb402d675c58bf18ce2d4048ab6feaa9334de33
Kiel Velasco <KVUni09@gmail.com>
2020-04-30 21:41:57 -0500

    finalizing

--- src/assetmanagementsystem/model/TechnologyType.java
+++ src/assetmanagementsystem/model/TechnologyType.java
@@ -1,19 +1,20 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package assetmanagementsystem.model;
 
 /**
  *
- * @author kyle
+ * @author Kiel Roi Velasco
  */
 public enum TechnologyType {
     COMPUTER("Computer"), PRINTER("Printer"), 
-    AUDIO_OR_VIDEO("Audio/Video"), UNDEFINED("Undefined");
+    AUDIO_OR_VIDEO("Audio/video"), UNDEFINED("Undefined");
     private final String name;
     TechnologyType(String name) {
         this.name = name;
     }
+
+    public String getName() {
+        return name;
+    }
+    
 }
